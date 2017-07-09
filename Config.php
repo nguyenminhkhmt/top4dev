380291f8ab09f6367b36ba5749e1aef1998c4503
minhndcs52 <nguyenminhkhmt@gmail.com>
2017-07-09 16:36:10 +0700

    [Init] Init core function & some feature.

---
+++ Config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'heroku_5f41ec68a00bbb4',
+    'username' => 'bff8df7535a470',
+    'password' => '787ea627',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'flrm_',
+    'strict' => false,
+  ),
+  'url' => 'http://top4dev.vn',
+  'paths' => 
+  array (
+    'api' => 'api',	
+    'admin' => 'admin',
+  ),
+);
